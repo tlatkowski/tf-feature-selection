51bf39bd85fbb50a3c344553eaeb2efe8368bdae
Tomasz Latkowski <t.latkowski@gmail.com>
2018-02-27 22:11:03 +0100

    added simple experiment config

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -2,11 +2,23 @@ import tensorflow as tf
 
 from experiments.classifier import NeuralNetworkClassifier
 from methods.selection_wrapper import SelectionWrapper
+from methods.selection import fisher, feature_correlation_with_class, t_test, random
+
+methods = {
+    'fisher': fisher,
+    'corr': feature_correlation_with_class,
+    'ttest': t_test,
+    'random': random
+}
 
 
 class Experiment:
 
-    def __init__(self, selection_method, num_features, num_instances, classifier, dataset):
+    def __init__(self, experiment_config, num_features, num_instances, classifier, dataset):
+
+        selection_method = methods[experiment_config['SELECTION']['method']]
+        num_features = experiment_config['SELECTION']['num_features']
+
         with tf.name_scope('selection'):
             self.selection_wrapper = SelectionWrapper(dataset,
                                                       num_instances=num_instances,
--- run.py
+++ run.py
@@ -1,52 +1,73 @@
+import configparser
+from argparse import ArgumentParser
+
 import tensorflow as tf
 from tqdm import tqdm
 
 from experiments.dataset import Dataset
 from experiments.experiment import Experiment
-from methods.selection import fisher
 from utils.log_saver import LogSaver
 
-dataset = Dataset('data/autism.tsv')
 
-num_features = 100
-num_epochs = 1000
-eval_every = 10
+def run_experiment(experiment_config):
+    dataset = Dataset('data/autism.tsv')
 
-for fold_id, (train_idxs, test_idxs) in dataset.cross_validation():
+    num_epochs = 1000
+    eval_every = 10
 
-    data_train_fold = dataset.get_data(train_idxs)
-    num_instances, labels_train_fold = dataset.get_labels(train_idxs)
+    for fold_id, (train_idxs, test_idxs) in dataset.cross_validation():
 
-    data_test_fold = dataset.get_data(test_idxs)
-    _, labels_test_fold = dataset.get_labels(test_idxs)
+        data_train_fold = dataset.get_data(train_idxs)
+        num_instances, labels_train_fold = dataset.get_labels(train_idxs)
 
-    with tf.Graph().as_default() as graph:
+        data_test_fold = dataset.get_data(test_idxs)
+        _, labels_test_fold = dataset.get_labels(test_idxs)
 
-        experiment = Experiment(fisher, num_features, num_instances, None, data_train_fold)
+        with tf.Graph().as_default() as graph:
 
-        with tf.Session() as session:
+            experiment = Experiment(experiment_config, num_instances, None, data_train_fold)
 
-            global_step = 0
-            session.run(tf.global_variables_initializer())
+            with tf.Session() as session:
 
-            log_saver = LogSaver('logs', 'fisher_fold{}'.format(fold_id), session.graph)
+                global_step = 0
+                session.run(tf.global_variables_initializer())
 
-            train_selected_data = session.run(experiment.selection_wrapper.selected_data)
-            test_selected_data = session.run(experiment.selection_wrapper.select(data_test_fold))
+                log_saver = LogSaver('logs', 'fisher_fold{}'.format(fold_id), session.graph)
 
-            tqdm_iter = tqdm(range(num_epochs), desc='Epochs')
+                train_selected_data = session.run(experiment.selection_wrapper.selected_data)
+                test_selected_data = session.run(experiment.selection_wrapper.select(data_test_fold))
 
-            for epoch in tqdm_iter:
-                feed_dict = {experiment.clf.x: train_selected_data, experiment.clf.y: labels_train_fold}
-                loss, _ = session.run([experiment.clf.loss, experiment.clf.opt],
-                                      feed_dict=feed_dict)
+                tqdm_iter = tqdm(range(num_epochs), desc='Epochs')
 
-                if epoch % eval_every == 0:
-                    summary = session.run(experiment.clf.summary_op, feed_dict=feed_dict)
-                    log_saver.log_train(summary, epoch)
+                for epoch in tqdm_iter:
+                    feed_dict = {experiment.clf.x: train_selected_data, experiment.clf.y: labels_train_fold}
+                    loss, _ = session.run([experiment.clf.loss, experiment.clf.opt],
+                                          feed_dict=feed_dict)
 
-                    feed_dict = {experiment.clf.x: test_selected_data, experiment.clf.y: labels_test_fold}
-                    summary = session.run(experiment.clf.summary_op, feed_dict=feed_dict)
-                    log_saver.log_test(summary, epoch)
+                    if epoch % eval_every == 0:
+                        summary = session.run(experiment.clf.summary_op, feed_dict=feed_dict)
+                        log_saver.log_train(summary, epoch)
 
-                tqdm_iter.set_postfix(loss='{:.2f}'.format(float(loss)), epoch=epoch)
+                        feed_dict = {experiment.clf.x: test_selected_data, experiment.clf.y: labels_test_fold}
+                        summary = session.run(experiment.clf.summary_op, feed_dict=feed_dict)
+                        log_saver.log_test(summary, epoch)
+
+                    tqdm_iter.set_postfix(loss='{:.2f}'.format(float(loss)), epoch=epoch)
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('experiment',
+                        default='simple_experiment',
+                        choices=['simple_experiment'],
+                        help='model used during training (default: %(default))')
+
+    args = parser.parse_args()
+    experiment_config = configparser.ConfigParser()
+    experiment_config.read('config/{}.ini'.format(args.experiment))
+
+    run_experiment(experiment_config)
+
+
+if __name__ == '__main__':
+    main()
